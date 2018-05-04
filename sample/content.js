2803ed79da3c0d3d1dd4722510edee7ebd45eb92
sundayliu <martinkro@aliyun.com>
2018-05-04 20:35:15 +0800

    v0.0.3-log external link url

--- sample/content.js
+++ sample/content.js
@@ -1,3 +1,7 @@
 // content.js
 
-alert("Hello from your Chrome extension!")
+// alert("Hello from your Chrome extension!")
+
+// we don’t need to use jQuery to check if the document has loaded. By default, Chrome injects content scripts after the DOM is complete.
+var firstHref = $("a[href^='http']").eq(0).attr("href");
+console.log(firstHref);
